3a3a4bc7e7d05d8b26ec30d8cecb916be2520e81
creation7654321 <creation.pradhan@gmail.com>
2024-08-23 14:52:09 +0545

    creating NewCollections component and and adding it into shop page

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
 
 export default function Shop() {
   return (
@@ -9,6 +10,7 @@ export default function Shop() {
       <Hero/>
       <Popular/>
       <Offers/>
+      <NewCollections/>
     </div>
   )
 }
