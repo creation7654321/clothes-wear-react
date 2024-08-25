dc1fd397504d5670941e4940c377db3af50fde66
creation7654321 <creation.pradhan@gmail.com>
2024-08-25 17:01:03 +0545

    creating description box for each realted product

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+export default function DescriptionBox() {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis quo facere obcaecati ab tempore mollitia nostrum! Cumque illo repellat, dolores perspiciatis pariatur necessitatibus deserunt laudantium inventore quisquam quos fugit!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda nesciunt autem reprehenderit blanditiis a quis, molestias porro eveniet quo labore vitae id, distinctio consequuntur quaerat excepturi explicabo minus quae. Deleniti, tempore asperiores!
+
+            </p>
+        </div>
+    </div>
+  )
+}
