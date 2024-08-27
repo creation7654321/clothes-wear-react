15a366e96a1644911ec6ceca83cbf5dc7f31ef14
creation7654321 <creation.pradhan@gmail.com>
2024-08-27 14:55:55 +0545

    creating the cart page

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -12,9 +12,6 @@ const getDefaultCart = ()=>{
     }
     return cart;
 }
-
-
-
 const ShopContextProvider = (props)=>{
     const [cartItems, setCartItems] = useState(getDefaultCart());
     
@@ -27,7 +24,18 @@ const ShopContextProvider = (props)=>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
         
     }
-    const contextValue = {all_product,cartItems,addToCart,removeFromCart};
+    const getTotalCartAmount = ()=>{
+        let totalAmount = 0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                let itemInfo = all_product.find((product)=>product.id === Number(item))
+                totalAmount += itemInfo.new_price * cartItems[item];
+            }
+            return totalAmount;
+        }
+    }
+  
+    const contextValue = {all_product,cartItems,addToCart,removeFromCart,getTotalCartAmount};
       
     return(
         <ShopContext.Provider value={contextValue}>
